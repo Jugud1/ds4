3c93a3d0b84142ba29cd1caab98f34ca67ef0b2c
JULIO GUDIÑO <julio.gudino1@utp.ac.pa>
2024-09-29 00:35:39 -0500

    laboratorio 3

---
+++ Laboratorio2/Laboratorio2/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using static laboratorio2.Program;
+
+
+namespace laboratorio2
+{
+    class Program
+    {
+       static void Main (string[] args)
+
+        {
+         Client client = new Client();
+            //Ejemplo utilizando las variables de instancia de clase.
+
+            client.FirstName = "Su-Nombre";
+            client.LastName = "Su_Apellido";
+            client.Age = 15;
+            client.Id = 1;
+
+            Console.WriteLine(client.GetFullName());
+            
+
+
+        }
+
+        public class Client
+        { 
+          public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public ushort Age { get; set; }
+
+            public string GetFullName()
+
+            {
+                return FirstName + " " + LastName;
+
+            }
+        
+          
+        
+        
+        }
+
+    }
+
+   
+
+
+
+}
+
+
+
+internal class Program
+{
+
+    private static void Main(string[] args)
+
+    {
+        Client client = new Client();
+        //Ejemplo utilizando las variables de instancia de clase.
+
+        client.FirstName = "Su-Nombre";
+        client.LastName = "Su_Apellido";
+        client.Age = 15;
+        client.Id = 1;
+
+        Console.WriteLine(client.GetFullName());
+
+    }
+
+
+    public class MyClass
+        {
+        public static int Valor;  // declarando variable estátca
+         
+               
+        }
+    
+    
+    public void Sumar(int valor1, int valor2)
+    {
+        int variablelocal = valor1 + valor2;
+        Console.WriteLine(variablelocal);
+
+    }
+
+
+
+
+}
---
+++ Laboratorio2/Laboratorio21/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+
+
+//namespace Laboratorio21
+//{
+    //public class Program
+  //  {
+       // public static void main()
+
+       // {
+
+            //Asignando valor a variable estatica
+           // MyClass.Valor = 1;
+            //Console.WriteLine(MyClass.Valor);
+
+
+       // }
+        //public class MyClass
+        //{
+            // Declarando Variable estática
+
+            //public static int Valor;
+
+        //}
+
+
+   // }
+
+    // PARTE 3
+
+    //int nombreVariable = 1000;
+   // int valor1 = 28;
+   // valor2 = valor1;
+   // valor1 = 30;
+
+
+     //Console.WriteLine(valor1);
+     //Console.WriteLine(valor2);
+     //Console.WriteLine(valor1);
+    // Console.WriteLine(valor2);
+
+
+   // MyClass object1 = new MyClass();  //creando una nueva instancia
+   // object1.Nombre = "Fulano";
+    //object1.Edad = 28;
+   // MyClass object2 = object1;  // Asignando una variable a otra
+
+
+   // MyClass object1 = new MyClass();  //creando una nueva instancia
+   // object1.Nombre = "Yeison";
+   // object1.Edad = 28;
+
+    // Asignando una variable a otra
+    // MyClass object2 = object1;  // Asignando una variable a otra
+
+    //este cambio en la propiedad afecta tanto a object1 como a object2
+    //object2.Nombre = "José";
+
+    // al imprimir en consola vemos que ambas referencias imprimen el mismo valor José
+
+    //Console.WriteLine(object2.Nombre);
+    //Console.WriteLine(object1.Nombre);
+
+
+    //Tipos de datos alfanumericos
+    //char caracter = 'A';
+    //string cadena = "cadena de caracteres"; //variable almacenada en una cadena de caracteres.
+
+   // bool condicion = true;
+
+
+
+
+
+
+
+
+
