a98855d3dbe1bed416b3939661e1fe0163183e31
JULIO GUDIÑO <julio.gudino1@utp.ac.pa>
2024-09-29 00:29:45 -0500

    laboratorio 8

---
+++ Laboratorio8.1/Laboratorio8.1/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio8._1
+{
+    class Persona
+    {
+        public string Nombre;
+        public int edad;
+        public string NIF;
+
+        void Cumpleaños()
+        {
+
+
+            edad++;
+        }
+
+        public Persona(string nombre, int edad, string nif)
+        {
+            Nombre = nombre;
+            edad = edad;
+            NIF = nif;
+        }
+    }
+}
---
+++ Laboratorio8.1/Laboratorio8.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using Laboratorio8._1;
+using System;
+
+
+class Program
+
+{
+    class Trabajador : Persona
+    {
+
+        public int Sueldo;
+
+        public Trabajador(string nombre, int edad, string nif, int sueldo)
+            : base(nombre, edad, nif)
+        {
+
+            Sueldo = sueldo;
+        }
+
+
+    }
+
+    public static void main()
+    {
+
+        Trabajador p = new Trabajador("Josan", 22, "77588260-z", 100000);
+        Console.WriteLine("Nombre=" + p.Nombre);
+        Console.WriteLine("Edad=" + p.edad);
+        Console.WriteLine("NIF=" + p.NIF);
+        Console.WriteLine("Sueldo=" + p.Sueldo);
+        Console.ReadKey();
+
+    }
+
+
+}
