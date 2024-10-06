72555423f284a22f42898856f9f53153e8ffe799
JULIO GUDIÑO <julio.gudino1@utp.ac.pa>
2024-10-05 20:35:33 -0500

    laboratorio 11

---
+++ Laboratorio3/Laboratorio3.2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+public class CalculosMatematicos
+{
+    //metodo para calcular circulo
+
+    public static double AreaCirculo(double c)
+
+    {
+        const double PI = Math.PI;
+        double area = PI * Math.Pow(c, 2);
+
+
+        return area;
+
+    }
+
+    //metodo para calcular
+    public static int Calcular(int a, int b)
+
+    {
+
+        return (a + b) * (a - b);
+
+    }
+
+}
+
+//metodo principal del programa
+internal class Program
+{
+    private static void Main(string[] args)
+
+    {
+        Console.Write("Ingrese el primer número: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.Write("Ingrese el segundo número: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        int suma = a + b;
+
+        Console.WriteLine("La suma de {0} y {1} es  {2}  ", a, b, suma);
+
+
+        int resultado = CalculosMatematicos.Calcular(a, b);
+
+        Console.WriteLine($"El resultado de la operación ({a} + {b}) * ({a} - {b}) es: {resultado}");
+
+    }
+
+
+}
---
+++ Laboratorio3/Laboratorio3.3/Program.cs
@@ -0,0 +1,45 @@
+﻿
+public class CalculadoraDeRectangulos
+{
+
+    public static double RectanguloArea(double alto, double ancho)
+
+    {
+
+
+
+        return alto * ancho;
+
+
+
+    }
+
+
+
+
+
+
+
+    internal class Rectangulo
+    {
+        public static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("INGRESA EL ALTO DEL RECTANGULO");
+            double alto = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("INGRESA EL ANCHO DEL RECTANGULO");
+            double ancho = Convert.ToDouble(Console.ReadLine());
+
+
+            double rectangulo = CalculadoraDeRectangulos.RectanguloArea(alto, ancho);
+
+            Console.WriteLine("EL AREA DEL RECTANGULO ES {0}:" , rectangulo );
+
+
+
+
+
+        }
+    }
+}
