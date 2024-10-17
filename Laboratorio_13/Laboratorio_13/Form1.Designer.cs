dfa2ed72692c2621196f940fba2e30d640094459
Jugud1 <julio.gudino1@utp.ac.pa>
2024-10-16 19:03:38 -0500

    Add files via upload
    LABORATORIO 13

---
+++ Laboratorio_13/Laboratorio_13/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Laboratorio_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHit = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnHit
+            // 
+            this.btnHit.Location = new System.Drawing.Point(242, 36);
+            this.btnHit.Name = "btnHit";
+            this.btnHit.Size = new System.Drawing.Size(311, 124);
+            this.btnHit.TabIndex = 0;
+            this.btnHit.Text = "Conectar y Desconectar SQL server ";
+            this.btnHit.UseVisualStyleBackColor = true;
+            this.btnHit.Click += new System.EventHandler(this.btnHit_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(242, 227);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(311, 180);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnHit);
+            this.Name = "Form1";
+            this.Text = "Conectar y desconectar  SQL server";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHit;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Laboratorio_13/Laboratorio_13/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Laboratorio_13
+{
+    public partial class Form1 : Form
+
+    {
+        string connectionString =
+            @"Server=.\sqlexpress;Database=Northwind;TrustServerCertificate=true;Integrated Security=SSPI;";
+        public Form1()
+        {
+           
+            InitializeComponent();
+        }
+
+        
+        private void btnHit_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear(); // Limpiar el ListBox antes de llenarlo
+
+            using (SqlConnection conexion = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conexion.Open();
+                    string query = "SELECT ProductName FROM [dbo].[Products]";
+                    SqlCommand command = new SqlCommand(query, conexion);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            listBox1.Items.Add(reader["ProductName"].ToString());
+                        }
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show($"Error al ejecutar la consulta: {ex.Message}");
+                }
+                finally
+                {
+                    if (conexion.State == ConnectionState.Open)
+                    {
+                        conexion.Close();
+                    }
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
